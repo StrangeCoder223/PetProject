6c0019de97846a286b528be384f99fea38862c77
StrangeCoder223 <unitydmitryj@gmail.com>
2021-08-27 17:41:18 +0500

    Added 2 states: alive and die

---
+++ Assets/Scripts/Units/State/AliveState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AliveState : State
+{
+    //“ут объ€вл€етс€ Movement и другие прелести живого персонажа
+    public override void Run()
+    {
+        
+    }
+}
