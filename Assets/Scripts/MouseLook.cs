97a5831c314214bf603f70419421ed0a1e132933
StrangeCoder223 <unitydmitryj@gmail.com>
2021-09-06 21:42:36 +0500

    Update MouseLook.cs

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,14 +6,32 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float sensetivity;
+    [SerializeField] private Transform character;
     private float _x, _y;
+
+    private void OnEnable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     private void Look()
     {
         _x += Input.GetAxis("Mouse X") * sensetivity;
         _y += Input.GetAxis("Mouse Y") * sensetivity;
-        _x = Mathf.Clamp(_x, -90f, 90f);
+        _y = Mathf.Clamp(_y, -90f, 90f);
         transform.rotation = Quaternion.Euler(-_y, _x, 0f);
+        RotateCharacter(character.rotation.x,_x);
+    }
+
+    private void RotateCharacter(float x, float y)
+    {
+        character.rotation = Quaternion.Euler(x,y,0f);
+    }
+
+    public Transform GetTarget()
+    {
+        return character;
     }
 
     void Update()
